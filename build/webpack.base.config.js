4c3d2158f3a8ba0e50880cd46fc1b66ee84bef7f
raniys_liao <raniys_liao@shrise.cn>
2017-05-25 18:01:10 +0800

    update

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -57,7 +57,9 @@ const config = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            'process.env': {
+                NODE_ENV: '"development"'
+            }
         })
     ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@
  * Created by raniys on 5/25/17.
  */
 
-process.env.NODE_ENV = 'development';
-
 const appConfig = require('./build/webpack.app.config');
 
 module.exports = appConfig;
